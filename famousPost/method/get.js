7566d9013a207e7bab43373261077243b3ee38d5
amitiwary99 <tiwarya9@gmail.com>
2020-12-03 23:04:38 +0530

    get the post event if user not logged in

--- famousPost/method/get.js
+++ famousPost/method/get.js
@@ -1,7 +1,7 @@
 const {fetchFamousPost} = require('../function/fetchFamousPosts')
 
 module.exports = async(req, res) => {
-    let userId = req.user.uid? req.user.uid: ""
+    let userId = req.user ? req.user.uid ? req.user.uid: "" : ""
     await fetchFamousPost(userId)
     .then(result => {
         res.status(200).send(result)
