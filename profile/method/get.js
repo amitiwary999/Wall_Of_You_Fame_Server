8651f8e0007fad34883b878c74aa44b7f75c2fcb
amitiwary99 <tiwarya9@gmail.com>
2020-12-15 23:29:13 +0530

    update code for fetch famous post

--- profile/method/get.js
+++ profile/method/get.js
@@ -1,7 +1,7 @@
 const {fetchProfile} = require('../functions/fetchUserProfile')
 
 module.exports = async(req, res) => {
-    let userId = res.user ? res.user.uid : ""
+    let userId = req.user ? req.user.uid : ""
     await fetchProfile(req.query, userId)
     .then(result => {
         res.status(200).send(result)
