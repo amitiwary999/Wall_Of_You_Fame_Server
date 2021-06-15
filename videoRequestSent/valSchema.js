c3d89cdaa4159d85738fb68509b8b34dad8e3802
amitiwary999 <tiwarya9@gmail.com>
2021-06-15 10:26:37 +0530

    fix: put the check for date time and the number in video request api

--- videoRequestSent/valSchema.js
+++ videoRequestSent/valSchema.js
@@ -1,10 +1,11 @@
-const Joi = require('joi'); 
+const Joi = require('joi')
+.extend(require('@joi/date'));
 
 const schema =  {
     postVideoCallRequest: Joi.object({
         inviteeId: Joi.string().required(),
-        status: Joi.string().required(),
-        callTime: Joi.string().required()
+        status: Joi.number().integer().required(),
+        callTime: Joi.date().format('YYYY-MM-DD HH:mm:ss').required()
     }),
     deleteVideoCallRequest: Joi.object({
         id: Joi.string().required()
