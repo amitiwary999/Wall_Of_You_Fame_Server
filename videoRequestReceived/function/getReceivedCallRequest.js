cdb34042451d41d16ba56d86d21ac02bb293973d
amitiwary999 <tiwarya9@gmail.com>
2021-06-20 17:51:20 +0530

    update the sql query to fetch received request

--- videoRequestReceived/function/getReceivedCallRequest.js
+++ videoRequestReceived/function/getReceivedCallRequest.js
@@ -1,13 +1,15 @@
-const {runQuery} = require('../../db/query')
+const {runQuery, getVideoCallRequestDb} = require('../../db/query')
 
 const getCallRequest = async(inviteeId) => {
-    let sql = "SELECT * FROM wallfame_video_requests_table INNER JOIN (SELECT userId, userName, userDp FROM wallfame_user_table)u ON u.userId = wallfame_video_requests_table.requestorId WHERE inviteeId = '"+ inviteeId+"' AND status != 2";
-    let result = await runQuery(sql);
-    if(result){
-        return result
-    }else{
-        throw "No video request"
-    }
+    return new Promise((resolve, reject) => {
+        getVideoCallRequestDb(inviteeId, (error, data) => {
+            if(error){
+                reject(new Error('No video request'))
+            }else{
+                resolve(data)
+            }
+        })
+    })
 }
 
 module.exports = {
